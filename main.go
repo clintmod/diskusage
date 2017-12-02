488c7f03a0920d56cd2464c18eb1a4e635083e8e
Clint Modien <cmodien@gmail.com>
2017-12-02 07:52:44 -0600

    I have the output the way I want it

--- main.go
+++ main.go
@@ -3,50 +3,53 @@ package main
 import (
 	"fmt"
 	"os"
-	"time"
-
-	"github.com/gosuri/uilive"
-	"github.com/stretchr/powerwalk"
+	"io/ioutil"
+	"log"
+	"path/filepath"
 )
 
-const kb = 1024.00
-
-var writer = uilive.New()
-var size float64
-var filecount uint64
-
 func main() {
-	defer timeTrack(time.Now(), "main")
-	writer.Start()
-	dirSize(os.Args[1])
-	writer.Stop()
+	dirList(os.Args[1])
 }
 
-func timeTrack(start time.Time, name string) {
-	elapsed := time.Since(start)
-	fmt.Printf("%s took %s\n", name, elapsed)
+func dirList(path string) {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if(!file.IsDir()) {
+			fmt.Printf("name: %s, size: %.2f KB\n", file.Name(), float64(file.Size()) / 1024)
+		} else {
+			var size, file_count = dirSize(path + "/" + file.Name())
+			var float_size float64 = float64(size) / 1024
+			var size_unit string = "KB"
+			if(float_size > 1024) {
+				float_size = float_size / 1024
+				size_unit = "MB"
+			}
+			if(float_size > 1024) {
+				float_size = float_size / 1024
+				size_unit = "GB"
+			}
+			fmt.Printf("name: %s, size: %.2f %s, count:%v\n", file.Name(), float_size, size_unit, file_count)
+		}
+	}
 }
 
-func dirSize(path string) (float64, error) {
-	err := powerwalk.Walk(path, func(_ string, info os.FileInfo, err error) error {
+func dirSize(path string)(int64, int64) {
+	var size int64
+	var file_count int64
+	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
-			size += float64(info.Size())
-			filecount++
-			log(size, filecount)
+			size += info.Size()
+			file_count++
 		}
 		return err
 	})
-	return size, err
-}
-
-var lastLogTime = time.Now()
-
-func log(size float64, filecount uint64) {
-	currentTime := time.Now()
-	if currentTime.Sub(lastLogTime) < time.Millisecond*110 {
-		return
+	if err != nil {
+		log.Fatal(err)
 	}
-	fmt.Fprintf(writer, "Size: %f GB | File count: %d\n", size/(kb*kb*kb), filecount)
-	writer.Flush()
-	lastLogTime = currentTime
+	return size, file_count
 }
