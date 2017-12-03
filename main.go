8ed466d8da110acaadbc6db3f9b9838f507343ad
Clint Modien <cmodien@gmail.com>
2017-12-03 09:57:43 -0600

    using readdirnames

--- main.go
+++ main.go
@@ -3,63 +3,29 @@ package main
 import (
 	"fmt"
 	"os"
-	"io/ioutil"
-	"log"
-	"path/filepath"
+	"sort"
 )
 
 func main() {
-	dirList(os.Args[1])
+	find(os.Args[1])
 }
 
-func dirList(path string) {
-	files, err := ioutil.ReadDir(path)
+func find(path string) {
+	f, err := os.Open(path)
+	defer f.Close()
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	for _, file := range files {
-		if(!file.IsDir()) {
-			var float_size, size_unit = getFileSize(file.Size())
-			fmt.Printf("name: %s, size: %.2f %v\n", file.Name(), float_size, size_unit)
-		} else {
-			var size, _ = dirSize(path + "/" + file.Name())
-			var float_size, size_unit = getFileSize(size)
-			fmt.Printf("name: %s, size: %.2f %s\n", file.Name(), float_size, size_unit)
-		}
-	}
-}
-
-func dirSize(path string)(int64, int64) {
-	var size int64
-	var file_count int64
-	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
-		if !info.IsDir() {
-			size += info.Size()
-			file_count++
-		}
-		return err
-	})
+	// Get file names
+	files, err := f.Readdirnames(0)
 	if err != nil {
-		log.Fatal(err)
+		return
 	}
-	return size, file_count
+	sort.Strings(files)
+	// show files
+	for _, v := range files {
+		fmt.Println(v)
+	}
 }
 
-func getFileSize(size int64)(float64, string){
-	var float_size float64 = float64(size)
-	var size_unit string = "B"
-	if(float_size > 1024) {
-		float_size = float_size / 1024
-		size_unit = "KB"
-	}
-	if(float_size > 1024) {
-		float_size = float_size / 1024
-		size_unit = "MB"
-	}
-	if(float_size > 1024) {
-		float_size = float_size / 1024
-		size_unit = "GB"
-	}
-	return float_size, size_unit
-}
