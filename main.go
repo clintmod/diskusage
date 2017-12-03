449a4bff20ea50a2a80bdbe6241e0db62f9cfe85
Clint Modien <cmodien@gmail.com>
2017-12-03 12:47:09 -0600

    trying fastwalk from github.com/golang/tools/imports/fastwalk.go

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"os"
-	"io/ioutil"
 	"log"
-	"path/filepath"
+	"sync"
+	"fmt"
+	"io/ioutil"
 )
 
 func main() {
@@ -17,49 +18,55 @@ func dirList(path string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	var float_size float64
+	var size_unit string
+	var file_name string
+	var file_path string
 	for _, file := range files {
-		if(!file.IsDir()) {
-			var float_size, size_unit = getFileSize(file.Size())
-			fmt.Printf("name: %s, size: %.2f %v\n", file.Name(), float_size, size_unit)
+		file_name = file.Name()
+		file_path = path + file_name
+		if (!file.IsDir()) {
+			float_size, size_unit = getFileSize(file.Size())
 		} else {
-			var size, _ = dirSize(path + "/" + file.Name())
-			var float_size, size_unit = getFileSize(size)
-			fmt.Printf("name: %s, size: %.2f %s\n", file.Name(), float_size, size_unit)
+			var size, _= dirSize(file_path)
+			float_size, size_unit = getFileSize(size)
 		}
+		fmt.Printf("%.2f%s\t%s\n", float_size, size_unit, file_path)
 	}
 }
 
-func dirSize(path string)(int64, int64) {
-	var size int64
-	var file_count int64
-	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
-		if !info.IsDir() {
-			size += info.Size()
-			file_count++
+func dirSize(path string) (int64, error) {
+	var mu sync.Mutex
+	var size int64 = 0
+	err := fastWalk(path, func(path string, typ os.FileMode) error {
+		mu.Lock()
+		defer mu.Unlock()
+		if !typ.IsDir() {
+			var f, err = os.Lstat(path)
+			if err != nil {
+				return err
+			}
+			size += f.Size()
 		}
-		return err
+		return nil
 	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	return size, file_count
+	return size, err
 }
 
 func getFileSize(size int64)(float64, string){
 	var float_size float64 = float64(size)
 	var size_unit string = "B"
 	if(float_size > 1024) {
-		float_size = float_size / 1024
-		size_unit = "KB"
-	}
+			float_size = float_size / 1024
+			size_unit = "K"
+		}
 	if(float_size > 1024) {
-		float_size = float_size / 1024
-		size_unit = "MB"
-	}
+			float_size = float_size / 1024
+			size_unit = "M"
+		}
 	if(float_size > 1024) {
-		float_size = float_size / 1024
-		size_unit = "GB"
-	}
+			float_size = float_size / 1024
+			size_unit = "G"
+		}
 	return float_size, size_unit
 }
